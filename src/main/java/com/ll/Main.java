78eb6c590a3a541e971567603a6d5323038cb895
YunJiW <dtroy371@gmail.com>
2023-04-19 14:16:57 +0900

    readme 수정

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,17 @@
 package com.ll;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayList<Integer> aa = new ArrayList<>();
+
+
     }
+}
+
+class MyArrayList<T> {
+
 }
