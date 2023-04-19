efeb84f7e5386bc146d518156129fbcabfaa1f32
YunJiW <dtroy371@gmail.com>
2023-04-19 14:33:01 +0900

    TDD 형식으로 진행 : addê¸가 될때 size가 +1이된다.

--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -1,14 +1,26 @@
 package com.ll.exam1;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyArrayListTest {
 
     @Test
-    void testSize() {
+    @DisplayName("testSize")
+    void t1() {
         MyArrayList<String> list = new MyArrayList<>();
-        assertEquals(0, list.size());
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testAdd() {
+        MyArrayList<String> list = new MyArrayList<>();
+        assertTrue(list.add("Element1"));
+        assertEquals(1, list.size());
     }
 }
