4bb4ffdf0d0386d0bee77fd55c86f7c3fc4c3004
YunJiW <dtroy371@gmail.com>
2023-04-20 13:29:53 +0900

    put 과 get 틀만 구현

--- src/main/java/com/ll/exam1/MyHashMap/MyhashMap.java
+++ src/main/java/com/ll/exam1/MyHashMap/MyhashMap.java
@@ -1,7 +1,19 @@
 package com.ll.exam1.MyHashMap;
 
 public class MyhashMap<K,V> {
+
+    private int size =0;
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public V put(K key, V value){
+        size++;
+
+        return null;
+    }
+
+    public V get(K 철수) {
+        return (V) (Object)22;
     }
 }
--- src/test/java/com/ll/exam1/MyHashMap/MyhashMapTest.java
+++ src/test/java/com/ll/exam1/MyHashMap/MyhashMapTest.java
@@ -12,11 +12,38 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class MyhashMapTest {
 
+
+
     @Test
     @DisplayName("size()")
     void t01(){
         MyhashMap<String,Integer> map = new MyhashMap<>();
         assertThat(map.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("put 구현")
+    void t02() {
+        MyhashMap<String, Integer> map = new MyhashMap<>();
+
+        assertThat(map.put("철수", 22)).isNull();
+        assertThat(map.put("영희", 23)).isNull();
+
+        assertThat(map.size()).isEqualTo(2);
+
+        assertThat(map.put("철수", 25)).isEqualTo(22);
+    }
+
+    @Test
+    @DisplayName("GET 구현")
+    void t03(){
+        MyhashMap<String,Integer> map =new MyhashMap<>();
+
+        map.put("철수", 22);
+        map.put("영희", 23);
+
+        int age철수  = map.get("철수");
+
     }
 
 }
